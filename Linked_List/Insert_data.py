fff1e4c17ae199f9cce27d5c2aa43b2ecb1f3663
yash-vij <yashvij1996@gmail.com>
2022-10-01 23:54:00 +0530

    Linked list insert data

--- Linked_List/Insert_data.py
+++ Linked_List/Insert_data.py
@@ -13,6 +13,15 @@ class linked_list:
             print(temp.data)
             temp = temp.next
 
-    #def inser_at_front(self,front) :
+    def inser_at_front(self,front_data) :
+        new_node = Node(front_data)
 
+        new_node.next = self.head
+        self.head = new_node
 
+llist = linked_list()
+llist.inser_at_front(3)
+llist.inser_at_front(4)
+llist.inser_at_front(5)
+llist.inser_at_front(6)
+llist.print_list()
