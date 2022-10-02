b814c0759b5081c3633bd01ad94a273a641642fc
yash-vij <yashvij1996@gmail.com>
2022-10-02 23:04:26 +0530

    Linked List insertion

--- Linked_List/Insert_data.py
+++ Linked_List/Insert_data.py
@@ -19,9 +19,32 @@ class linked_list:
         new_node.next = self.head
         self.head = new_node
 
+    def insert_after(self, previous, new_data):
+        count = 0
+        if previous == None:
+            print("Previous cannot be empty")
+            return
+
+        new_node =  Node(new_data)
+        new_node.next = previous.next
+        previous.next = new_node
+
+    def insert_last(self, new_data):
+        ele = llist.head
+        while(ele.next != None):
+            ele = ele.next
+        new_node = Node(new_data)
+        ele.next = new_node
+
+
 llist = linked_list()
 llist.inser_at_front(3)
 llist.inser_at_front(4)
 llist.inser_at_front(5)
 llist.inser_at_front(6)
+llist.insert_after(llist.head.next.next, 100)
+llist.insert_after(llist.head.next.next, 1345)
+llist.insert_last(45)
+llist.insert_last(98)
 llist.print_list()
+
