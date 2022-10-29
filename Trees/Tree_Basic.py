c669074b3063f8e4bb7a98ef10132bb1b108e178
yash-vij <yashvij1996@gmail.com>
2022-10-29 07:31:48 +0530

    Basic Trees

--- Trees/Tree_Basic.py
+++ Trees/Tree_Basic.py
@@ -1,6 +1,10 @@
-class node:
+class Node:
     def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
 
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
